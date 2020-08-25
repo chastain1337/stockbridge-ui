5ada717c6ce11b5070419e67ab6931d2d927cda9
James Chastain <jchastain@strategicmarketing.com>
2020-08-25 14:52:35 -0400

    editable grid toolbar

--- src/Shared/Components/EditableGrid.vue
+++ src/Shared/Components/EditableGrid.vue
@@ -1,34 +1,74 @@
+Using this component:
+  - Register it in the parent
+  - Pass in the props as detailed below
+  - In the parent you need to handle @dataChange:. 
+    - This event passes in a new data array (includes blank rows as data)
+    - You must at least:
+      -Set your local totalRows field to max(data.length,current totalRows) 
+      -Set local data = data parameter that was passed in.
+
+
+
 <template>
-  <table class="table table-bordered table-sm table-hover table-striped">
-    <thead>
-      <tr>
-        <th v-for="col in columns" :key="col.field">{{ col.title }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(row,rowIndex) in allData" :key="`rw-${rowIndex}`" :data-row-index="rowIndex">
-        <td
-          v-for="(col,colIndex) in columns"
-          @paste="handlePaste"
-          @blur="cellBlurred($event.target.textContent,rowIndex,col.field,colIndex)"
-          :key="`rowIndex-${col.field}`"
-          :data-row-index="rowIndex"
-          :data-col-index="colIndex"
-          :data-cell-coordinates="`${rowIndex},${colIndex}`"
-          :data-col-field="col.field"
-          contenteditable="true"
-        >
-          <template v-if="allData[rowIndex][col.field]">{{allData[rowIndex][col.field]}}</template>
-          <br v-else />
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="container-fluid">
+    <div class="row text-center border-bottom pb-3 border-dark">
+      <div class="col-2" v-if="buttonsToInclude.includes('ROWCOUNT') || buttonsToInclude.includes('ALL')">
+        Total Rows:
+        <kendo-numerictextbox
+          class="ml-2"
+          v-model.number="totalRowsLocal"
+          :max="500"
+          :min="1"
+          :decimals="0"
+          :format="'#'"
+          :disabled="!!this.allData"
+        ></kendo-numerictextbox>
+      </div>
+      <div class="col-1" v-if="buttonsToInclude.includes('SUBMIT') || buttonsToInclude.includes('ALL')">
+        <button class="btn btn-success" @click="handleSubmit" :disabled="!this.allData">Submit</button>
+      </div>
+      <div class="col-1" v-if="buttonsToInclude.includes('VALIDATE') || buttonsToInclude.includes('ALL')" >
+        <button class="btn btn-warning" @click="handleValidate" :disabled="!this.allData">Validate</button>
+      </div>
+      <div class="col-1" v-if="buttonsToInclude.includes('COPY') || buttonsToInclude.includes('ALL')" >
+       <button class="btn btn-primary" @click="handleCopy" :disabled="!this.allData">Copy</button>
+      </div>
+      <div class="col-1" v-if="buttonsToInclude.includes('CLEAR') || buttonsToInclude.includes('ALL')">
+        <button class="btn btn-danger" @click="handleClear" :disabled="!this.allData">Clear</button>
+      </div>
+    </div >
+    <div class="row" v-if="!!this.allData">
+      <table class="table table-bordered table-sm table-hover table-striped">
+        <thead>
+          <tr>
+            <th v-for="col in columns" :key="col.field">{{ col.title }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row,rowIndex) in allData" :key="`rw-${rowIndex}`" :data-row-index="rowIndex">
+            <td
+              v-for="(col,colIndex) in columns"
+              @paste="handlePaste"
+              @blur="cellBlurred($event.target.textContent,rowIndex,col.field,colIndex,$event.target)"
+              :key="`${rowIndex}-${col.field}-${allData[rowIndex][col.field]}`"
+              :data-row-index="rowIndex"
+              :data-col-index="colIndex"
+              :data-cell-coordinates="`${rowIndex},${colIndex}`"
+              :data-col-field="col.field"
+              contenteditable="true"
+            >
+              <template v-if="allData[rowIndex][col.field]">{{allData[rowIndex][col.field]}}</template>
+              <br v-else />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 import { required } from "vuelidate/lib/validators";
-import entityValidations from "@/Shared/validations";
 import gridEntityData from "@/Shared/gridEntityData";
 
 export default {
@@ -36,16 +76,30 @@ export default {
   data() {
     return {
       allData: null,
+      totalRowsLocal: this.totalRows,
     };
   },
   props: {
-    columns: { type: Array, required: true }, // title: string, field: string, editor?: defaults to text, can pass in HTML or Vue object
-    data: { type: Array, required: true }, // must pass in empty array if only blank rows
-    totalRows: { type: Number, required: true }, // generates blank rows after data rows using the columns provided.
+    columns: {required: true }, // title: string, field: string, editor?: defaults to text, can pass in HTML or Vue object
+    data: { required: true }, // must pass in empty array if only blank rows
+    totalRows: { type: Number, required: true }, // generates blank rows after data rows using the columns provided. Sets the initial value of the selector (see enableRowCountEdit)
     validations: Object, // vuelidate validations object, see /shared/validations.js for examples
-    enableBulkPaste: { type: Boolean, default: false }, // whether paste events should be auto-parsed
+    enableBulkPaste: { type: Boolean, default: false }, // whether paste events should be auto-parsed and spread across the grid
+    buttonsToInclude: {type: Array, default: []} // toolbar buttons to include. Valid values are: "ROWCOUNT","SUBMIT","VALIDATE","COPY","CLEAR". Can also use "ALL" to enable all buttons
   },
   methods: {
+    handleSubmit() {
+
+    },
+    handleCopy() {
+
+    },
+    handleValidate() {
+
+    },
+    handleClear() {
+      this.updateParent(this.emptyRows(this.totalRowsLocal))
+    },
     handlePaste(e) {
       if (!this.enableBulkPaste) return;
       e.stopPropagation();
@@ -53,16 +107,16 @@ export default {
 
       // Get pasted data via clipboard API
       const clipboardData = e.clipboardData || window.clipboardData;
-      const pastedData = clipboardData
+      let pastedData = clipboardData
         .getData("Text")
         .split("\n")
         .map((row) => row.split(/\t/));
-      console.log(e);
       const startingColField = e.target.getAttribute("data-col-field");
       const startingColIndex = Number(e.target.getAttribute("data-col-index"));
       const startingRow = Number(e.target.getAttribute("data-row-index"));
 
-      console.log(pastedData);
+      // Remove any completely blank rows
+      pastedData = pastedData.filter((row) => row.every((cell) => cell !== ""));
 
       // Make sure data fits in columns
       const remainingColumns = this.columns.length - startingColIndex;
@@ -73,10 +127,10 @@ export default {
       }
 
       // Strech all data to fit rows
-      const rowsRemaining =
-        this.allData.length - (pastedData.length + startingRow);
+      const rowsToAdd =
+        startingRow + pastedData.length - 1 - this.allData.length;
       let newData = [...this.allData];
-      if (rowsRemaining < 0) {
+      if (rowsToAdd > 0) {
         const emptyRows = this.emptyRows(rowsToAdd);
         newData = [...this.allData, ...emptyRows];
       }
@@ -94,38 +148,52 @@ export default {
         newData[currentRow] = { ...thisRow };
         currentRow++;
       });
-      this.allData = [...newData];
-      this.$emit("dataChange", newData);
+
+      this.totalRowsLocal = newData.length;
+      this.updateParent(newData);
     },
-    cellBlurred(newValue, rowIndex, colField, colIndex) {
+    cellBlurred(newValue, rowIndex, colField, colIndex, e) {
       if (this.allData[rowIndex][colField] !== newValue && newValue !== "") {
         const thisRow = { ...this.allData[rowIndex] };
         const dataCopy = [...this.allData];
         thisRow[colField] = newValue;
         dataCopy[rowIndex] = thisRow;
 
-        console.log(rowIndex);
-        console.log(dataCopy);
         // emit event to change data in parent component
-        this.$emit("dataChange", dataCopy);
+
+        this.updateParent(dataCopy);
       }
     },
     emptyRows(count = 1) {
       let emptyRow = {};
       this.columns.forEach((col) => (emptyRow[col.field] = null));
-      return Array(count).fill(emptyRow); // I know, I know...
+      return Array(count).fill({ ...emptyRow }); // I know, I know...
+    },
+    createAllData() {
+      const emptyRows = this.emptyRows(this.totalRows - this.data.length);
+      const dataCopy = [...this.data, ...emptyRows];
+      this.allData = dataCopy;
+    },
+    updateParent(newData) {
+      //const newestData = newData.filter( row => Object.values(row).some( cell => cell !== "" && cell !== null));
+      this.$emit("dataChange", newData);
     },
   },
   mounted() {
-    const emptyRows = this.emptyRows(this.totalRows - this.data.length);
-    const dataCopy = [...this.data, ...emptyRows];
-    this.allData = dataCopy;
+    if (this.data) this.createAllData();
+  },
+  watch: {
+    data() {
+      if (this.data === null) return this.allData = null;
+      this.createAllData();
+    },
   },
   validations() {
+    if (!this.validations) return {};
     return {
       data: {
         required,
-        $each: validations,
+        $each: this.validations,
       },
     };
   },
--- src/components/Tools/BulkImporter.vue
+++ src/components/Tools/BulkImporter.vue
@@ -1,46 +1,8 @@
 <template>
   <div class="container-fluid mt-2">
-    <k-dialog v-if="modalVisible" :modal="true" @close="closeModal" :width="800">
-      <div class="container-fluid my-2">
-        <div class="row">
-          <div class="col text-center">
-            <textarea
-              style="width: 720px"
-              v-model="pastedText"
-              placeholder="Paste tab separated data here, including 'headers' row..."
-            ></textarea>
-          </div>
-        </div>
-      </div>
-      <div class="row mx-2">
-        <div class="col">Paste into / starting at row:</div>
-      </div>
-      <div class="row mx-2">
-        <div class="col">
-          <kendo-numerictextbox
-            v-model.number="pasteIntoRow"
-            :max="1000"
-            :min="1"
-            :decimals="0"
-            :format="'#'"
-          ></kendo-numerictextbox>
-        </div>
-      </div>
-
-      <dialog-actions-bar>
-        <button
-          class="k-button"
-          style="background-color: #81AE9D"
-          @click="handlePaste"
-          :disabled="this.pastedText.trim().length === 0"
-        >Insert</button>
-        <button class="k-button" style="background-color: #dddddd" @click="closeModal">Cancel</button>
-      </dialog-actions-bar>
-    </k-dialog>
-
     <h1>Bulk Importer</h1>
-    <div class="row mb-4">
-      <div class="col-3">
+    <div class="row mb-2">
+      <div class="col-2">
         <kendo-dropdownlist
           :data-source="bulkImportOptions"
           optionLabel="Entity to Import"
@@ -49,45 +11,21 @@
           data-value-field="storeAction"
         ></kendo-dropdownlist>
       </div>
-      <div class="col-5">
-        Blank Rows:
-        <kendo-numerictextbox
-          class="ml-2"
-          v-model.number="blankRows"
-          :max="500"
-          :min="1"
-          :decimals="0"
-          :format="'#'"
-          :disabled="!!gridData"
-        ></kendo-numerictextbox>
-      </div>
       <div class="col-3">
         <button class="btn btn-outline-danger" @click="resetGrid" :disabled="!gridData">Reset</button>
-        <button
-          class="btn btn-outline-success"
-          type="button"
-          :disabled="!gridData"
-          @click="openModal"
-        >Bulk Paste</button>
-        <button class="btn btn-outline-warning" type="button" :diabled="!gridData">Validate Grid</button>
-        <!-- <button
-          class="btn btn-success"
-          type="button"
-          :disabled="$v.gridData.$anyInvalid"
-          @click="submit"
-        >Import Data</button>-->
       </div>
     </div>
 
     <div class="row">
       <div class="col">
         <grid
-          v-if="gridData"
           :enableBulkPaste="true"
           :data="gridData"
           :columns="gridColumns"
-          :totalRows="gridData.length + blankRows"
+          :totalRows="totalRows"
           @dataChange="dataChange"
+          :validations="entityValidations"
+          :buttonsToInclude="['ALL']"
         />
       </div>
     </div>
@@ -97,6 +35,7 @@
 <script>
 import gridEntityData from "@/Shared/gridEntityData";
 import EditableGrid from "@/shared/Components/EditableGrid";
+import allValidations from "@/shared/validations"
 
 export default {
   name: "BulkImporter",
@@ -113,14 +52,16 @@ export default {
       selectedEntityStoreAction: "",
       gridData: null,
       gridColumns: null,
-      blankRows: 5,
+      totalRows: 5,
       pastedText: "",
       modalVisible: false,
       pasteIntoRow: 1,
+      entityValidations: null
     };
   },
   methods: {
     dataChange(gridData) {
+      this.totalRows = Math.max(gridData.length,this.totalRows)
       this.gridData = gridData;
     },
     openModal() {
@@ -129,68 +70,6 @@ export default {
     closeModal() {
       this.modalVisible = false;
     },
-    handlePaste() {
-      this.closeModal();
-
-      const pastedRows = this.pastedText
-        .split("\n")
-        .map((row) => row.split(/\t/));
-
-      const pastedHeaders = [...pastedRows[0]];
-      const pastedHeaderGridColumnIndexes = pastedHeaders.map((pHeader) =>
-        this.gridColumns.findIndex((h) => h.title === pHeader)
-      );
-      if (
-        pastedRows.length === 1 ||
-        pastedHeaderGridColumnIndexes.every((idx) => idx < 0)
-      ) {
-        return this.$store.commit("notify", {
-          message:
-            "You either pasted no data, no header row, or all headers are invalid. Correct and try again.",
-          callback: this.openModal,
-        });
-      }
-
-      pastedRows.splice(0, 1); // remove header row and any entirely blank rows
-      let i = 0;
-      pastedRows.forEach((row) => {
-        if (row.every((c) => c === "")) {
-          pastedRows.splice(i, 1);
-        }
-        i++;
-      });
-      const blankRow = {};
-      this.gridColumns.forEach((c) => (blankRow[c.field] = ""));
-      const totalNewRows = pastedRows.length;
-      const newRowsToAdd =
-        totalNewRows - (this.blankRows - this.pasteIntoRow + 1);
-
-      // Stretch grid data to fit the rows that we need
-      let gridDataCopy = [...this.gridData];
-      for (let i = 1; i <= newRowsToAdd; i++) {
-        gridDataCopy.push(blankRow);
-      }
-
-      let currentRow = this.pasteIntoRow - 1; // zero based index
-
-      pastedRows.forEach((pastedRow) => {
-        const thisRow = { ...gridDataCopy[currentRow] };
-        let i = 0;
-        pastedRow.forEach((pastedCol) => {
-          const thisHeaderIdx = pastedHeaderGridColumnIndexes[i];
-          if (thisHeaderIdx >= 0) {
-            // check if header is valid, add to error list if not
-            thisRow[this.gridColumns[thisHeaderIdx].field] = pastedCol;
-            i++;
-          }
-        });
-        gridDataCopy[currentRow] = { ...thisRow };
-        currentRow++;
-      });
-
-      this.blankRows = gridDataCopy.length;
-      this.gridData = gridDataCopy;
-    },
     resetGrid() {
       this.selectedEntityStoreAction = "";
       this.gridData = null;
@@ -198,7 +77,7 @@ export default {
     },
     refreshGridAfterDataLoad() {
       // Does not update the store with new data
-
+      let thisValidationObject;
       let sampleData = {};
       let columns = [];
       switch (this.selectedEntityStoreAction) {
@@ -218,17 +97,10 @@ export default {
               sampleData[field] = null;
             }
           }
+          thisValidationObject = allValidations.employees;
       }
 
-      let emptyRows = [];
-
-      for (let i = 1; i <= this.blankRows; i++) {
-        const thisDatum = { ...sampleData };
-        thisDatum.id = i;
-        thisDatum.inEdit = i === this.editID;
-        emptyRows.push(thisDatum);
-      }
-
+      this.entityValidations = thisValidationObject;
       this.gridColumns = columns;
       this.gridData = [];
       // this.$v.gridData.touch();
@@ -254,5 +126,9 @@ td.invalid-e {
 td.invalid-o {
   color: rgb(151, 0, 0);
   background-color: rgb(248, 135, 154);
+}
+
+.k-numerictextbox {
+  width: 100px;
 }
 </style>
